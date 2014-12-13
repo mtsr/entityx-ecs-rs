8747aeaad96fea1311dbc18b5b58d45eae30b08c
Jonas Matser <jonas@jonasmatser.nl>
2014-12-13 17:11:50 +0100

    Added Control, EntityBuilder and EntityModifier Added Control to system update

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,78 @@ use std::uint;
 use std::intrinsics::TypeId;
 use std::fmt::Show;
 
+pub trait EntityBuilder: 'static
+{
+    fn build(&mut self, &mut EntityManager, Entity);
+}
+
+impl EntityBuilder for |&mut EntityManager, Entity|: 'static
+{
+    fn build(&mut self, c: &mut EntityManager, e: Entity)
+    {
+        (*self)(c, e);
+    }
+}
+
+pub trait EntityModifier: 'static
+{
+    fn modify(&mut self, &mut EntityManager, Entity);
+}
+
+impl EntityModifier for |&mut EntityManager, Entity|: 'static
+{
+    fn modify(&mut self, c: &mut EntityManager, e: Entity)
+    {
+        (*self)(c, e);
+    }
+}
+
+pub struct Control<'a> {
+    builders: Vec<Box<EntityBuilder + 'static>>,
+    destroyed: Vec<Entity>,
+    modifiers: Vec<(Entity, Box<EntityModifier + 'static>)>,
+}
+
+impl<'a> Control<'a> {
+    fn new() -> Control<'a> {
+        Control {
+            builders: Vec::new(),
+            destroyed: Vec::new(),
+            modifiers: Vec::new(),
+        }
+    }
+
+    pub fn build(&mut self, builder: Box<EntityBuilder + 'static>) {
+        self.builders.push(builder);
+    }
+
+    pub fn destroy(&mut self, entity: Entity) {
+        self.destroyed.push(entity);
+    }
+
+    pub fn modify(&mut self, entity: Entity, modifier: Box<EntityModifier + 'static>) {
+        self.modifiers.push((entity, modifier));
+    }
+
+    fn apply(self, entity_manager: &Rc<RefCell<EntityManager>>) {
+        let mut entity_manager = entity_manager.borrow_mut();
+        for mut builder in self.builders.into_iter() {
+            let entity = entity_manager.deref_mut().create_entity();
+            builder.build(entity_manager.deref_mut(), entity);
+        }
+
+        for (entity, mut modifier) in self.modifiers.into_iter() {
+            modifier.modify(entity_manager.deref_mut(), entity);
+        }
+
+        for entity in self.destroyed.into_iter() {
+            entity_manager.deref_mut().destroy_entity(entity);
+        }
+    }
+}
+
 pub trait System {
-    fn update<A>(&self, entities: Rc<RefCell<EntityManager>>, args: &A) where A: Show;
-    fn update<A>(&mut self, entities: Rc<RefCell<EntityManager>>, args: &A) where A: Show;
+    fn update<A>(&mut self, entity_manager: &Rc<RefCell<EntityManager>>, &mut Control, args: &A) where A: Show;
 }
 
 pub struct SystemManager {
@@ -32,12 +101,12 @@ impl SystemManager {
         self.systems.insert(system);
     }
 
-    pub fn update<A, S>(&self, entities: Rc<RefCell<EntityManager>>, args: &A) where S: System + 'static, A: Show {
-        match self.systems.get::<S>() {
-    pub fn update<A, S>(&mut self, entities: Rc<RefCell<EntityManager>>, args: &A) where S: System + 'static, A: Show {
+    pub fn update<A, S>(&mut self, entity_manager: &Rc<RefCell<EntityManager>>, args: &A) where S: System + 'static, A: Show {
         match self.systems.get_mut::<S>() {
             Some(system) => {
-                system.update(entities, args);
+                let mut control = Control::new();
+                system.update(entity_manager, &mut control, args);
+                control.apply(entity_manager);
             },
             None => panic!("Tried to update unregistered system")
         }
