85151c24139e4ce960d9baf163f6b4784173f129
Jonas Matser <jonas@jonasmatser.nl>
2014-12-21 16:49:57 +0100

    Add tests directory

---
+++ tests/lib.rs
@@ -0,0 +1,18 @@
+extern crate test;
+
+extern crate ecs;
+
+use ecs::{
+    Entity,
+    Control,
+    EntityManager,
+    System,
+    SystemManager,
+    TupAppend, // required for components macro
+    ComponentDatastructure,
+};
+
+#[test]
+fn tests_directory_works() {
+    assert!(true);
+}
